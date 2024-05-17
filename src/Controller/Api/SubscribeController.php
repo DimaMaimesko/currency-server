7cf561fed60212e8c71b129a10e8886efb927073
dmytromaimesko <dima.maimesko@gmail.com>
2024-05-17 17:17:32 +0300

    fix

--- src/Controller/Api/SubscribeController.php
+++ src/Controller/Api/SubscribeController.php
@@ -5,11 +5,10 @@ namespace App\Controller\Api;
 use App\Model\Entities\Email;
 use App\Model\Entities\Subscriber;
 use App\Repository\SubscribersRepository;
-use App\Services\CurrencyConvertors\ChainConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
-
+use Symfony\Component\HttpFoundation\JsonResponse;
 class SubscribeController extends AbstractController
 {
     public function __construct(
@@ -19,7 +18,7 @@ class SubscribeController extends AbstractController
     }
 
     #[Route('/api/subscribe', name: 'subscribe_user', methods: ['POST'])]
-    public function rates(Request $request)
+    public function subscribe(Request $request): JsonResponse
     {
         $emailFromRequest = $request->request->get("email");
         $email = new Email($emailFromRequest);
